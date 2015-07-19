3fd740bc63f21c20bd5887bdff0dd022b29f4092
Alfred Shi <memoriedsea@gmail.com>
2015-07-20 02:33:23 +0800

    update logic

--- Monkey010/src/me/alfredis/monkey010/Tutorial.java
+++ Monkey010/src/me/alfredis/monkey010/Tutorial.java
@@ -7,18 +7,30 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.net.wifi.WifiInfo;
+import android.os.Build;
 import android.os.Environment;
+import android.provider.Settings;
 import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
 import android.widget.TextView;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+
 
 public class Tutorial implements IXposedHookLoadPackage {
 	private HashMap<String, String> configMap = new HashMap<String, String>();
@@ -33,7 +45,7 @@ public class Tutorial implements IXposedHookLoadPackage {
                     break;
                 }
                 
-                line = line.substring(1, line.length() - 2);
+                line = line.substring(1, line.length() - 1);
                 String[] lineArray = line.split(",");
                 for (String str : lineArray) {
 
@@ -60,7 +72,7 @@ public class Tutorial implements IXposedHookLoadPackage {
                 	}
 
 
-                    XposedBridge.log(tempKey + " " + tempValue);
+                    //XposedBridge.log(tempKey + " " + tempValue);
                     configMap.put(tempKey, tempValue);
                 }
 			}
@@ -75,7 +87,7 @@ public class Tutorial implements IXposedHookLoadPackage {
 	}
 	
 	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-		//XposedBridge.log("Loaded app: " + lpparam.packageName);
+		XposedBridge.log("Loaded app: " + lpparam.packageName);
 		
 		
 		findAndHookMethod(TelephonyManager.class.getName(), lpparam.classLoader, "getDeviceId", new XC_MethodHook() {
@@ -189,6 +201,127 @@ public class Tutorial implements IXposedHookLoadPackage {
         		param.setResult(configMap.get("getSubscriberId"));
             }
         });
+		
+		de.robv.android.xposed.XposedHelpers.setStaticObjectField(Build.class, "BRAND", configMap.get("BRAND"));
+		de.robv.android.xposed.XposedHelpers.setStaticObjectField(Build.class, "DEVICE", configMap.get("DEVICE"));
+		de.robv.android.xposed.XposedHelpers.setStaticObjectField(Build.class, "FINGERPRINT", configMap.get("FINGERPRINT"));
+		de.robv.android.xposed.XposedHelpers.setStaticObjectField(Build.class, "HARDWARE", configMap.get("HARDWARE"));
+		de.robv.android.xposed.XposedHelpers.setStaticObjectField(Build.class, "MANUFACTURER", configMap.get("MANUFACTURER"));
+		de.robv.android.xposed.XposedHelpers.setStaticObjectField(Build.class, "MODEL", configMap.get("MODEL"));
+		de.robv.android.xposed.XposedHelpers.setStaticObjectField(Build.class, "PRODUCT", configMap.get("PRODUCT"));
+		de.robv.android.xposed.XposedHelpers.setStaticObjectField(Build.VERSION.class, "RELEASE", configMap.get("RELEASE"));
+		de.robv.android.xposed.XposedHelpers.setStaticObjectField(Build.VERSION.class, "SDK", configMap.get("SDK"));
+
+		String archTempString = configMap.get("ARCH");
+		de.robv.android.xposed.XposedHelpers.setStaticObjectField(Build.class, "CPU_ABI", archTempString.substring(0, archTempString.indexOf("_")));
+		de.robv.android.xposed.XposedHelpers.setStaticObjectField(Build.class, "CPU_ABI2", archTempString.substring(archTempString.indexOf("_") + 1, archTempString.length()));
+
+		//XposedHelpers.setFloatField(XposedHelpers.findClass("DisplayMetrics", lpparam.classLoader), "density", Float.valueOf(configMap.get("density")));
+		
+		
+		findAndHookMethod(Display.class.getName(), lpparam.classLoader, "getMetrics", DisplayMetrics.class.getName(), new XC_MethodHook() {
+        	@Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+        		super.afterHookedMethod(param);
+        		DisplayMetrics metrics = (DisplayMetrics) param.args[0];
+        		metrics.density = Float.valueOf(configMap.get("density"));
+        		metrics.densityDpi = Integer.valueOf(configMap.get("densityDpi"));
+        		metrics.scaledDensity = Float.valueOf(configMap.get("scaledDensity"));
+        		metrics.heightPixels = Integer.valueOf(configMap.get("getMetrics").substring(0, configMap.get("getMetrics").indexOf("x")));
+        		metrics.widthPixels = Integer.valueOf(configMap.get("getMetrics").substring(configMap.get("getMetrics").indexOf("x") + 1));
+        		metrics.xdpi = Float.valueOf(configMap.get("xdpi"));
+        		metrics.ydpi = Float.valueOf(configMap.get("ydpi"));
+        		param.setResult(metrics);
+        		}
+        });
+		
+		findAndHookMethod(TelephonyManager.class.getName(), lpparam.classLoader, "getSimState", new XC_MethodHook() {
+        	@Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+        		super.afterHookedMethod(param);
+        		param.setResult(Integer.valueOf(configMap.get("getSimState")));
+            }
+        });
+		
+		findAndHookMethod(TelephonyManager.class.getName(), lpparam.classLoader, "getNetworkType", new XC_MethodHook() {
+        	@Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+        		super.afterHookedMethod(param);
+        		param.setResult(Integer.valueOf(configMap.get("getNetworkType")));
+            }
+        });
+		
+		findAndHookMethod(TelephonyManager.class.getName(), lpparam.classLoader, "getPhoneType", new XC_MethodHook() {
+        	@Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+        		super.afterHookedMethod(param);
+        		param.setResult(Integer.valueOf(configMap.get("getPhoneType")));
+            }
+        });
+		
+		findAndHookMethod(Build.class.getName(), lpparam.classLoader, "getRadioVersion", new XC_MethodHook() {
+        	@Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+        		super.afterHookedMethod(param);
+        		param.setResult(configMap.get("getRadioVersion"));
+            }
+        });
+		
+		findAndHookMethod(GsmCellLocation.class.getName(), lpparam.classLoader, "getLac", new XC_MethodHook() {
+        	@Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+        		super.afterHookedMethod(param);
+        		param.setResult(Integer.parseInt(configMap.get("getJiZhan").substring(0, configMap.get("getJiZhan").indexOf("_"))));
+            }
+        });
+		
+		findAndHookMethod(GsmCellLocation.class.getName(), lpparam.classLoader, "getCid", new XC_MethodHook() {
+        	@Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+        		super.afterHookedMethod(param);
+        		param.setResult(Integer.parseInt(configMap.get("getJiZhan").substring(configMap.get("getJiZhan").indexOf("_") + 1)));
+            }
+        });
+		
+		findAndHookMethod(Settings.Secure.class.getName(), lpparam.classLoader, "getString", ContentResolver.class.getName(), String.class.getName(), new XC_MethodHook() {
+        	@Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+        		super.afterHookedMethod(param);
+        		if (((String) param.args[1]).endsWith("android_id")) {
+            		param.setResult(configMap.get("getString"));
+        		}
+            }
+        });
+		
+		Class systemPropertiesClass = null;
+        Object systemProperties = null;
+        Method systemPropertiesGetMethod = null;
+        try {
+            systemPropertiesClass = Class.forName("android.os.SystemProperties");
+            systemProperties = systemPropertiesClass.newInstance();
+            Class[] argClasses = new Class[2];
+            argClasses[0] = String.class;
+            argClasses[1] = String.class;
+            systemPropertiesGetMethod = systemPropertiesClass.getMethod("get", argClasses);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
         
+		findAndHookMethod(systemPropertiesClass.getName(), lpparam.classLoader, systemPropertiesGetMethod.getName(), String.class.getName(), String.class.getName(), new XC_MethodHook() {
+        	@Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+        		super.afterHookedMethod(param);
+        		if (((String) param.args[0]).endsWith("gsm.version.baseband") && ((String) param.args[1]).endsWith("no message")) {
+            		param.setResult(configMap.get("get"));
+        		}
+            }
+        });
+		
 	}
 }
