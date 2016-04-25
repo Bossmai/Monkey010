de42414ee678b6ce5f1c46e8de39a178c373605b
Alfred Shi <memoriedsea@gmail.com>
2016-04-25 14:30:20 +0800

    add gsm geo support..

--- Monkey010/app/src/main/java/me/alfredis/monkey010/Tutorial.java
+++ Monkey010/app/src/main/java/me/alfredis/monkey010/Tutorial.java
@@ -59,7 +59,7 @@ public class Tutorial implements IXposedHookLoadPackage {
 
     public static int cellIndex = 0;
 
-    private static final String configJson = "{\"connect_mode\":\"0\",\"density\":\"2.0\",\"densityDpi\":\"320\",\"get\":\"I9250XXLJ1\",\"getBSSID\":\"15:f8:96:bc:92:70\",\"getDeviceId\":\"990002043896289\",\"getJiZhan\":\"43016_11021269\",\"getLine1Number\":\"15435925392\",\"getMacAddress\":\"84:7a:88:ac:4a:da\",\"getMetrics\":\"720x1184\",\"getNetworkCountryIso\":\"cn\",\"getNetworkOperator\":\"46002\",\"getNetworkOperatorName\":\"中国移动\",\"getNetworkType\":\"1\",\"getPhoneType\":\"02\",\"getRadioVersion\":\"I9250XXLJ1\",\"getSSID\":\"home\",\"getSimCountryIso\":\"cn\",\"getSimOperator\":\"46002\",\"getSimOperatorName\":\"中国移动\",\"getSimSerialNumber\":\"89867785196673925881\",\"getSimState\":\"0\",\"getString\":\"c6fdfa76d49732ce\",\"getSubscriberId\":\"460029291155269\",\"gps\":null,\"location_mode\":\"0\",\"scaledDensity\":\"2.0\",\"setCpuName\":\"Tuna\",\"sign\":\"718E95ABAA307C583CFC5A9EAA5FB73E\",\"xdpi\":\"315.31033\",\"ydpi\":\"318.7451\",\"ARCH\":\"armeabi-v7a_armeabi\",\"BRAND\":\"oneplus\",\"DEVICE\":\"HTC ONE M8\",\"FINGERPRINT\":\"oneplus/bacon/A0001:5.0.2/LRX22G/YNG1TAS0YL:user/release-keys\",\"HARDWARE\":\"Qualcomm MSM8974PRO-AC\",\"MANUFACTURER\":\"OnePlus\",\"MODEL\":\"A0001\",\"PRODUCT\":\"A0001\",\"RELEASE\":\"4.3\",\"SDK\":\"18\",\"latitude\":\"0\",\"longitude\":\"0\"}";
+    private static final String configJson = "{\"connect_mode\":\"0\",\"density\":\"2.0\",\"densityDpi\":\"320\",\"get\":\"I9250XXLJ1\",\"getBSSID\":\"15:f8:96:bc:92:70\",\"getDeviceId\":\"990002043896289\",\"getJiZhan\":\"9,11031,3189\",\"getLine1Number\":\"15435925392\",\"getMacAddress\":\"84:7a:88:ac:4a:da\",\"getMetrics\":\"720x1184\",\"getNetworkCountryIso\":\"cn\",\"getNetworkOperator\":\"46002\",\"getNetworkOperatorName\":\"中国移动\",\"getNetworkType\":\"1\",\"getPhoneType\":\"02\",\"getRadioVersion\":\"I9250XXLJ1\",\"getSSID\":\"home\",\"getSimCountryIso\":\"cn\",\"getSimOperator\":\"46002\",\"getSimOperatorName\":\"中国移动\",\"getSimSerialNumber\":\"89867785196673925881\",\"getSimState\":\"0\",\"getString\":\"c6fdfa76d49732ce\",\"getSubscriberId\":\"460029291155269\",\"gps\":null,\"location_mode\":\"0\",\"scaledDensity\":\"2.0\",\"setCpuName\":\"Tuna\",\"sign\":\"718E95ABAA307C583CFC5A9EAA5FB73E\",\"xdpi\":\"315.31033\",\"ydpi\":\"318.7451\",\"ARCH\":\"armeabi-v7a_armeabi\",\"BRAND\":\"oneplus\",\"DEVICE\":\"HTC ONE M8\",\"FINGERPRINT\":\"oneplus/bacon/A0001:5.0.2/LRX22G/YNG1TAS0YL:user/release-keys\",\"HARDWARE\":\"Qualcomm MSM8974PRO-AC\",\"MANUFACTURER\":\"OnePlus\",\"MODEL\":\"A0001\",\"PRODUCT\":\"A0001\",\"RELEASE\":\"4.3\",\"SDK\":\"18\",\"latitude\":\"0\",\"longitude\":\"0\"}";
 
     private static final String drivers_content = "/dev/tty             /dev/tty        5       0 system:/dev/tty\n" +
             "/dev/console         /dev/console    5       1 system:console\n" +
@@ -367,7 +367,7 @@ public class Tutorial implements IXposedHookLoadPackage {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 super.afterHookedMethod(param);
-                param.setResult("46001");
+                param.setResult(configMap.get("getNetworkOperator"));
             }
         });
 
@@ -400,7 +400,7 @@ public class Tutorial implements IXposedHookLoadPackage {
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 super.afterHookedMethod(param);
 
-                param.setResult(3189);
+                param.setResult(Integer.parseInt(configMap.get("getJiZhan").split(",")[1]));
             }
         });
 
@@ -408,8 +408,7 @@ public class Tutorial implements IXposedHookLoadPackage {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 super.afterHookedMethod(param);
-                int[] cidArray = {11031, 35292, 9853};
-                param.setResult(cidArray[cellIndex++]);
+                param.setResult(Integer.parseInt(configMap.get("getJiZhan").split(",")[2]));
             }
         });
 
@@ -417,7 +416,7 @@ public class Tutorial implements IXposedHookLoadPackage {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 super.afterHookedMethod(param);
-                param.setResult(9);
+                param.setResult(Integer.parseInt(configMap.get("getJiZhan").split(",")[0]));
             }
         });
 
@@ -484,24 +483,6 @@ public class Tutorial implements IXposedHookLoadPackage {
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 super.afterHookedMethod(param);
                 param.setResult(configMap.get("getRadioVersion"));
-            }
-        });
-
-        findAndHookMethod(GsmCellLocation.class.getName(), lpparam.classLoader, "getLac", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                super.afterHookedMethod(param);
-                param.setResult(3189);
-                //param.setResult(Integer.parseInt(configMap.get("getJiZhan").substring(0, configMap.get("getJiZhan").indexOf("_"))));
-            }
-        });
-
-        findAndHookMethod(GsmCellLocation.class.getName(), lpparam.classLoader, "getCid", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                super.afterHookedMethod(param);
-                param.setResult(14452);
-                //param.setResult(Integer.parseInt(configMap.get("getJiZhan").substring(configMap.get("getJiZhan").indexOf("_") + 1)));
             }
         });
 
